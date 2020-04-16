e04546a186079ef862f7d1f1ce28bd22873d5d42
Alex <alex@smart-coffee-maker.rh.ncsu.edu>
2020-04-15 21:15:06 -0400

    Added Global variabled for width and height

--- conf.lua
+++ conf.lua
@@ -1,5 +1,10 @@
+local config = {}
 io.stdout:setvbuf("no")
+config.WIDTH = 1000 
+config.HEIGHT = 500
 function love.conf(t)
-	t.window.width = 550                -- The window width (number)
-    t.window.height = 550               -- The window height (number)
+	t.window.width = config.WIDTH                -- The window width (number)
+    t.window.height = config.HEIGHT               -- The window height (number)
+    t.window.title = "Very Good Pong"
 end
+return config
