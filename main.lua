bfe890ccb0f5f0e3491c056c0db0e93ef15e4645
Alex <alex@smart-coffee-maker.rh.ncsu.edu>
2020-04-15 21:16:05 -0400

    Collision fix + pause after score
    fixed the collision detection by using a much simpler function
    the game now freezes and repositions the pong after a score

--- main.lua
+++ main.lua
@@ -1,32 +1,46 @@
 Object = require "classic"
 require "paddle"
 require "pong"
-
+config = require 'conf'
+local function displayMSG(msg, y, f)
+	love.graphics.print(msg, config.WIDTH/2 - (f:getWidth(msg))/2,y)
+end
 function love.load() 
-	player = Paddle(25,550,15,100,'up','down')
-	enemy = Paddle(525,550,15,100,'w','s')
+	player = Paddle(25,config.HEIGHT,15,100,'up','down')
+	enemy = Paddle(config.WIDTH - 35,config.HEIGHT,15,100,'w','s')
 	pong = Pong()
 	player:setSpeed(200)
 	enemy:setSpeed(200)
 	pong:setSpeed(200)
 	font = love.graphics.newFont("Zilap Sleep Demo.ttf", 60)
+	msgFont = love.graphics.newFont("Zilap Sleep Demo.ttf", 20)
+	frozen = true
 end
+local function checkCol(e1, e2)
+	return e1.x < e2.x+e2.w and
+		   e2.x < e1.x+e1.w and
+		   e1.y < e2.y+e2.h and
+		   e2.y < e1.y+e1.h
+  end
+
 function love.update(dt)
 	player:update(dt)
-	pong:update(dt)
 	enemy:update(dt)
-	print(player.speed)
-	if pong.x < player.x+player.w and pong.y >= player.y and pong.y <= player.y+player.h then 
-		pong.xv = pong.xv * -1
+	if not frozen then
+		pong:update(dt)
+		if  checkCol(player, pong) then
+			pong.xv = pong.xv * -1
+			pong.x = player.x + player.w + 1
+		end
+		if checkCol(enemy, pong) then
+			pong.xv = pong.xv * -1
+			pong.x = enemy.x - 1 - pong.w
+		end
+	else
+		if love.keyboard.isDown("space") then
+			frozen = false
+		end
 	end
-	if pong.x + pong.w > enemy.x and pong.y >= enemy.y and pong.y <= enemy.y+enemy.h then -- Bug the player 2 can use the force to control the pong???
-		pong.xv = pong.xv * -1
-	end
-	-- To do: add collision on the sides. 
-	if pong.x > player.x and pong.x < player.x - player.w and pong.y > player.y and pong.y < player.y + player.h then
-		pong.yv = pong.yv * -1
-	end
-
 end
 function love.keyreleased(key)
 	player:setSpeed(200)
@@ -37,8 +51,18 @@ function love.draw()
 	-- love.graphics.setColor(0, 0, 255, 255)
 	love.graphics.setFont(font)
 	score = tostring(pong.p2Score) .. " : " .. tostring(pong.p1Score)
-    love.graphics.print(score, 225,100)
+	-- love.graphics.print(score, config.WIDTH/2 - (font:getWidth(score))/2,100)
+	displayMSG(score, 100, font)
+	if frozen then
+		love.graphics.setFont(msgFont)
+
+		displayMSG("Press space to start...", config.HEIGHT/2 + 100, msgFont)
+	end
+	love.graphics.setColor(255, 0, 0, 255)
 	player:draw()
-	pong:draw()
+	love.graphics.setColor(0, 0, 255, 255)
 	enemy:draw()
+	love.graphics.setColor(255, 255, 255, 255)
+	pong:draw()
+
 end
